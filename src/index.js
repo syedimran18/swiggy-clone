b1caebf034f82873a276514b902627f577bfded9
Syed Imran <102895175+syedimran18@users.noreply.github.com>
2024-04-03 19:08:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+
 // import Next from "next.js"
-// import this dir from "this directory dose not exist"
+import this dir from "this directory dose not exist"
 // this is a known error 
 
 ReactDOM.render(
